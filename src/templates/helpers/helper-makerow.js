b48b8b1e74cbef2fff19a41dabd11732608b6522
Tan Vo <tan.m.vo@gmail.com>
2014-07-12 15:20:19 +1000

    Update typeface and project structure

---
+++ src/templates/helpers/helper-makerow.js
@@ -0,0 +1,7 @@
+module.exports.register = function(Handlebars, options) {
+	Handlebars.registerHelper('makerow', function(context, options) {
+		// for (i = 0; i < context.length; i++) {
+		// console.log(projects[i] + ", " + projects[i += 1]);
+		return str;
+	});
+};
