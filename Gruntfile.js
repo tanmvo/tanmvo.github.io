39abc7e06dc03dbbdb5594aa82c67571fb127588
Tan Vo <tan.m.vo@gmail.com>
2017-04-25 17:48:46 +1000

    Refactored folders to new IA

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,125 +5,82 @@ module.exports = function(grunt) {
 		site: grunt.file.readYAML('_config.yaml'),
 		
 		assemble: {
-		
-		  projects: {
-	        src: ['<%= site.projects %>'],
-	        dest: '<%= site.dest %>/projects/',
-	        options: {
-		        flatten: true,
-		        assets: '<%= site.assets %>',
-		        data: '<%= site.data %>/*.{json,yml}',
+		  posts: {
+        src: ['<%= site.posts %>'],
+        dest: '<%= site.dest %>/posts/',
+        options: {
+	        flatten: true,
+	        assets: '<%= site.assets %>',
+	        data: '<%= site.data %>/*.{json,yml}',
 
-		        // Templates
-		        partials: '<%= site.includes %>',
-		        layoutdir: '<%= site.layouts %>',
-		        layout: 'project.hbs',
+	        // Templates
+	        partials: '<%= site.includes %>',
+	        layoutdir: '<%= site.layouts %>',
+	        layout: 'post.hbs',
 	    	},
-	      },
+	    },
 
-	      site: {
-	        src: ['<%= site.pages %>'],
-	        dest: '<%= site.dest %>',
-	        options: {
-		        flatten: true,
-		        assets: '<%= site.assets %>',
-		        data: '<%= site.data %>/*.{json,yml}',
+      site: {
+        src: ['<%= site.pages %>'],
+        dest: '<%= site.dest %>',
+        options: {
+	        flatten: true,
+	        assets: '<%= site.assets %>',
+	        data: '<%= site.data %>/*.{json,yml}',
 
-		        // Templates
-		        partials: '<%= site.includes %>',
-		        layoutdir: '<%= site.layouts %>',
-		        layout: '<%= site.layout %>',
-		        collections: [{
-		        	name: 'project',
-		        	sortby: 'order',
-		        	sortorder: 'ascending',
-		        }]
-	        },
-	      },
-
+	        // Templates
+	        partials: '<%= site.includes %>',
+	        layoutdir: '<%= site.layouts %>',
+	        layout: '<%= site.layout %>',
+        },
+      },
 		},
 
-		// browserSync: {
-			
-		// 	bsFiles: {
-		//         src : ['<%= site.pages %>', '<%= site.dest %>/projects/*.html', '<%= site.assets %>/css/*.css']
-		//     },
-		    
-		//     options: {
-		//         server: {
-		//             baseDir: "<%= site.dest %>",
-		//             tunnel: false,
-		//             injectChanges: true,
-		//         },
-		//         watchTask: true,
-		//     }
-
-		// },
-
 		clean: {
-			server: ['<%= assemble.projects.dest %>', '<%= site.assets %>', '!<%= site.assets %>' ]
+			server: ['<%= assemble.posts.dest %>', '<%= site.assets %>', '!<%= site.assets %>' ]
 		},
 
 		concurrent: {
-			server: ['sass', 'assemble:site', 'assemble:projects'],
+			server: ['sass', 'assemble:site', 'assemble:posts'],
 		},
 
 		connect: {
-	      options: {
-	        port: 9000,
-	        livereload: 35729,
-	        // change this to '0.0.0.0' to access the server from outside
-	        hostname: 'localhost'
-	      },
-	      livereload: {
-	        options: {
-	          open: true,
-	          base: [
-	            '<%= site.dest %>'
-	          ]
-	        }
-	      }
-	    },
+      options: {
+        port: 9000,
+        livereload: 35729,
+        // change this to '0.0.0.0' to access the server from outside
+        hostname: 'localhost'
+      },
+      livereload: {
+        options: {
+          open: true,
+          base: [
+            '<%= site.dest %>'
+          ]
+        }
+      }
+    },
 
-	    copy: {
-	    	main: {
-	    		expand: true,
-	    		cwd: './bower_components/bootstrap/dist/js/',
-	    		src: '**',
-	    		dest: '<%= site.assets %>/js/',
-	    		flatten: true
-	    	}
-	    },
+    copy: {
+    	main: {
+    		expand: true,
+    		cwd: './bower_components/bootstrap/dist/js/',
+    		src: '**',
+    		dest: '<%= site.assets %>/js/',
+    		flatten: true
+    	}
+    },
 
-	    sass: {
-	    	dist: {
-	    		options: {
-	    			style: 'expanded'
-	    		},
-	    		files: {
-	    			'./assets/css/main.css': './src/sass/main.scss'
-	    		}
-	    	}
-	    },
-
-		replace: {
-			// dist: {
-			// 	options: {
-			// 		patterns: [
-			// 			{
-			// 				match: "href=\"/\"",
-			// 				replacement: "href=\"http://www.tanmvo.com\""
-			// 			}
-			// 		]
-			// 	},
-			// 	files: {
-			// 		expand: true,
-			// 		flatten: true,
-			// 		src: ['<%= site.dest %>/index.html'],
-			// 		dest: '<%= site.dest %>'
-			// 	}
-			// }
-		},
+    sass: {
+    	dist: {
+    		options: {
+    			style: 'expanded'
+    		},
+    		files: {
+    			'./assets/css/main.css': './src/sass/main.scss'
+    		}
+    	}
+    },
 
 		watch: {
 			options: {
@@ -131,7 +88,7 @@ module.exports = function(grunt) {
 			},
 			
 			assemble: {
-				files: ['<%= site.pages %>', '<%= site.data %>/*.json', '<%= site.includes %>', '<%= site.projects %>' ],
+				files: ['<%= site.pages %>', '<%= site.data %>/*.json', '<%= site.includes %>', '<%= site.posts %>' ],
 				tasks: ['assemble']
 			},
 
@@ -150,7 +107,7 @@ module.exports = function(grunt) {
 		        },
 		        files: [
 		          '<%= site.dest %>/*.html',
-		          '<%= site.projects %>/*.html',
+		          '<%= site.posts %>/*.html',
 		          '<%= site.assets %>/css/*.css',
 		          '<%= site.assets %>/img/*.{gif,jpg,jpeg,png,svg,webp}'
 		        ]
